eacd4c94be66c369d2f5713efda702f5f4609694
Sebastiano <sebastianolicciardello@tutanota.com>
2023-09-16 11:36:03 +0200

    first commit

---
+++ app/pippo/page.tsx
@@ -0,0 +1,7 @@
+export default function PippoPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Pippo</h1>
+    </main>
+  );
+}
---
+++ app/pippo/pluto/page.tsx
@@ -0,0 +1,7 @@
+export default function PlutoPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Pluto</h1>
+    </main>
+  );
+}
