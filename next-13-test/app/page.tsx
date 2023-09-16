7344af6d4679466ca6b32cc74956470ac91c07f0
Sebastiano <sebastianolicciardello@tutanota.com>
2023-09-16 17:46:49 +0200

    initialization pages and navbar

---
+++ next-13-test/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
+      <h1 className="text-4xl font-bold">Home</h1>
+      <p className=" text-gray-700 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet nulla quis tempus ultricies.</p>
+    </main>
+  );
+}
